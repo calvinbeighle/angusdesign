9a110d51be623b7b790811be5842c41b40820a83
calvinbeighle <calvinbeighle@college.harvard.edu>
2024-10-24 12:49:18 -0700

    psychecare

--- pay/public/checkout.js
+++ pay/public/checkout.js
@@ -23,7 +23,17 @@ async function initialize() {
 
   const appearance = {
     theme: 'stripe',
+    variables: {
+      colorPrimary: '#000000', // Customize your primary color
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    },
+    rules: {
+      '.Input': {
+        iconColor: 'transparent', // Hides the card icons
+      },
+    },
   };
+
   elements = stripe.elements({ appearance, clientSecret });
 
   const paymentElementOptions = {
@@ -49,11 +59,6 @@ async function handleSubmit(e) {
     },
   });
 
-  // This point will only be reached if there is an immediate error when
-  // confirming the payment. Otherwise, your customer will be redirected to
-  // your `return_url`. For some payment methods like iDEAL, your customer will
-  // be redirected to an intermediate site first to authorize the payment, then
-  // redirected to the `return_url`.
   if (error.type === "card_error" || error.type === "validation_error") {
     showMessage(error.message);
   } else {
